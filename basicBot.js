212899dc16fc270cacea63ddff992a6322a79152
cade1306 <cade1305@live.co.uk>
2015-04-10 18:15:19 +0100

    Update basicBot.js

--- basicBot.js
+++ basicBot.js
@@ -193,7 +193,7 @@
         settings: {
             botName: "basicBot",
             language: "english",
-            chatLink: "https://rawgit.com/Yemasthui/basicBot/master/lang/en.json",
+            chatLink: "https://rawgit.com/cade1306/basicBot/master/lang/en.json",
             startupCap: 1, // 1-200
             startupVolume: 0, // 0-100
             startupEmoji: false, // true or false
